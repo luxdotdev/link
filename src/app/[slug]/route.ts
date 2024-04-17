154d744c24203085601b4001e7583a09fac5cb1b
Lucas Doell <lucas@lux.dev>
2024-04-17 00:47:47 -0400

    Refactor route.ts to include redirect logging

--- src/app/[slug]/route.ts
+++ src/app/[slug]/route.ts
@@ -2,10 +2,10 @@ import { kv } from "@vercel/kv";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(req: NextRequest) {
-  console.log(req.nextUrl.pathname);
-
   const slug = req.nextUrl.pathname;
   const url = await kv.get<string>(`link${slug}`);
+
+  console.log(`Redirecting ${slug} to ${url}...`);
 
   if (!url) {
     return NextResponse.redirect("https://parsertime.app/404", { status: 302 });
